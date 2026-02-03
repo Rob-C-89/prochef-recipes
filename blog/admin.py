5b1ebefabf8dc923aa11b482cb0aa01d78415497
Rob-C-89 <robbiecornell@gmail.com>
2026-02-03 13:17:30 +0000

    Add summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import RecipePost, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(RecipePost)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug',)
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('recipe_content',)
 
 # Register your models here.
-admin.site.register(RecipePost)
 admin.site.register(Comment)
+
