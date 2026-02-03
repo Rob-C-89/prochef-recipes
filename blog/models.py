2cf3c8ad45dd29ca405498e305787973c636be8c
Rob-C-89 <robbiecornell@gmail.com>
2026-02-03 11:34:11 +0000

    Create views, url and HTML templates

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,10 @@ class RecipePost(models.Model):
     )
     # Image code to go here
     recipe_content = models.TextField()
-    date_created = models.DateField(auto_now_add=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["-date_created"]
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
@@ -26,6 +29,9 @@ class Comment(models.Model):
         User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"Comment {self.body} | by {self.author}"
