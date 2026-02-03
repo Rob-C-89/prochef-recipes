aad995a534eaaf586bb79f5d9329eda54db554bf
Rob-C-89 <robbiecornell@gmail.com>
2026-02-03 15:26:49 +0000

    Create Post Detail function and template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import RecipePost
 
@@ -7,3 +7,13 @@ from .models import RecipePost
 class PostList(generic.ListView):
     queryset = RecipePost.objects.all()
     template_name = "blog/index.html"
+
+def post_detail(request, slug):
+    queryset = RecipePost.objects.all()
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
