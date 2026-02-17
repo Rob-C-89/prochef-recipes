55b441c55a89f6d89cf64d62cd611b493953a0e0
Rob-C-89 <robbiecornell@gmail.com>
2026-02-17 12:48:50 +0000

    Update Create Profile view, install Crispy, modify Create Profile and My Profile HTML

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from blog.models import RecipePost
+from .forms import UserProfileForm
 
 # Create your views here.
 
 @login_required
 def create_profile(request):
-    """Display the profile creation form"""
-    return render(request, 'profiles/create_profile.html')
+    # If user already has a profile, redirect to their profile page
+    if hasattr(request.user, 'profile_name'):
+        return redirect('my_profile')
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, request.FILES)
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('my_profile')
+    else:
+        profile_form = UserProfileForm()
+    return render(request, 'profiles/create_profile.html', {'profile_form': profile_form})
+
+
 
 # View own profile as logged in user.
 @login_required
