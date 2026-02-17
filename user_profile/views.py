96d9e3d6d8b69650749af818e38871744d87acb3
Rob-C-89 <robbiecornell@gmail.com>
2026-02-17 11:46:55 +0000

    Add initial Create User Profile view, template, form and URL

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from blog.models import RecipePost
 
 # Create your views here.
 
+@login_required
+def create_profile(request):
+    """Display the profile creation form"""
+    return render(request, 'profiles/create_profile.html')
+
 # View own profile as logged in user.
 @login_required
 def my_profile(request):
     """Display the logged-in user's profile"""
-    profile = request.user.profile_name
+    try:
+        profile = UserProfile.objects.get(user=request.user)
+    except UserProfile.DoesNotExist:
+        return redirect('create_profile')
     
     # Get all posts by this user
     user_posts = RecipePost.objects.filter(author=request.user).order_by('-date_created')
