385cb77dc8b53695867b32a20a6f441704c41e19
Rob-C-89 <robbiecornell@gmail.com>
2026-02-17 15:29:29 +0000

    Add Leave a Comment to Postlist view, post_detail.html and forms.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
-from .models import RecipePost
+from .models import RecipePost, Comment
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -12,8 +13,29 @@ def post_detail(request, slug):
     queryset = RecipePost.objects.all()
     post = get_object_or_404(queryset, slug=slug)
 
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = comments.count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', slug=post.slug)
+    else:
+        comment_form = CommentForm()
+
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
     )
+
+
